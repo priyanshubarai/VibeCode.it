8d2a45bf3d89ba7f5fc2365b75adbd6fb2cc9212
priyanshubarai <priyanshubarai45@gmail.com>
2025-11-25 14:18:00 +0530

    deploy fix generate command added

---
+++ auth.config.ts
@@ -0,0 +1,13 @@
+import Google from "next-auth/providers/google";
+import Github from "next-auth/providers/github";
+
+import type { NextAuthConfig } from "next-auth";
+import GitHub from "next-auth/providers/github";
+
+export default{
+    Providers:[
+        GitHub({
+            
+        })
+    ]
+}
